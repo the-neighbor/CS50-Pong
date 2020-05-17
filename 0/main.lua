8dad9f36f96d27ee5feb1fa46f022b0a73d91e74
Austin Daly, The Neighbor <austin-cd@posteo.org>
2020-05-16 22:57:46 -0400

    first two steps

---
+++ 0/main.lua
@@ -0,0 +1,17 @@
+WINDOW_WIDTH = 1280
+WINDOW_HEIGHT = 720
+
+function love.load()
+   love.window.setMode(WINDOW_WIDTH, WINDOW_HEIGHT, {
+      fullscreen = false,
+      vsync = true,
+      resizable = false
+   })
+end
+
+function love.update(dt)
+end
+
+function love.draw()
+	 love.graphics.printf("Hello Pong!", 0, WINDOW_HEIGHT / 2 - 6, WINDOW_WIDTH, 'center')
+end
